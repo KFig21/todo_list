1eaed6a38801d4343de2fee680418db0b048e314
Kevin <kfig21@gmail.com>
2021-05-20 11:05:19 -0400

    remove firebase

--- script.js
+++ script.js
@@ -742,25 +742,25 @@ function deleteProject () {
 
 // firebase
 
-var signInButtonElement = document.getElementById('sign-in');
-signInButtonElement.addEventListener('click', signIn);
+// var signInButtonElement = document.getElementById('sign-in');
+// signInButtonElement.addEventListener('click', signIn);
 
-function signIn() {
-    // Sign into Firebase using popup auth & Google as the identity provider.
-    var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  }
+// function signIn() {
+//     // Sign into Firebase using popup auth & Google as the identity provider.
+//     var provider = new firebase.auth.GoogleAuthProvider();
+//     firebase.auth().signInWithPopup(provider);
+//   }
 
-function signOut() {
-    // Sign out of Firebase.
-    firebase.auth().signOut();
-}
+// function signOut() {
+//     // Sign out of Firebase.
+//     firebase.auth().signOut();
+// }
 
-function initFirebaseAuth() {
-// Listen to auth state changes.
-    firebase.auth().onAuthStateChanged(authStateObserver);
-}
+// function initFirebaseAuth() {
+// // Listen to auth state changes.
+//     firebase.auth().onAuthStateChanged(authStateObserver);
+// }
 
-function isUserSignedIn() {
-    return !!firebase.auth().currentUser;
-}
+// function isUserSignedIn() {
+//     return !!firebase.auth().currentUser;
+// }

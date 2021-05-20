dd224791a4a7a1d882b1da2ba88d106fe9ced6e6
Kevin <kfig21@gmail.com>
2021-05-20 10:40:04 -0400

    add firebase

--- script.js
+++ script.js
@@ -738,3 +738,29 @@ function deleteProject () {
         confirmationDiv.appendChild(noButton);
     taskInputContainer.appendChild(confirmationDiv);
 }
+
+
+// firebase
+
+var signInButtonElement = document.getElementById('sign-in');
+signInButtonElement.addEventListener('click', signIn);
+
+function signIn() {
+    // Sign into Firebase using popup auth & Google as the identity provider.
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider);
+  }
+
+function signOut() {
+    // Sign out of Firebase.
+    firebase.auth().signOut();
+}
+
+function initFirebaseAuth() {
+// Listen to auth state changes.
+    firebase.auth().onAuthStateChanged(authStateObserver);
+}
+
+function isUserSignedIn() {
+    return !!firebase.auth().currentUser;
+}
